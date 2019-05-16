2a900e44582f3485a474af3f8b381ffc7a0818c3
22pop.fm <panpakapan0326@gmail.com>
2019-05-16 15:42:15 +0900

    マージずみ

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
 
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   resources :favorites, only:[:create,:destroy]
 
   root to:'toppages#index'
